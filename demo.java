79822561e4008b7717ede219951d1717470b3717
rekhaswaroopa <38184109+rekhaswaroopa@users.noreply.github.com>
2024-09-02 23:28:17 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         // Print a message to the console
         System.out.println("Hello from demo.java! hello guys how are you");
+        System.out.println("added");
     }
 }
